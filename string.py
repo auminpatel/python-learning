ebd6c0f17c3f8b7520954385ffb534ae80d8673b
Aumin Patel <auminpatel007@gmail.com>
2026-01-27 00:27:59 +0530

    chore: add

--- string.py
+++ string.py
@@ -2,14 +2,19 @@ course = "Python for 'Beginners'"
 
 course = 'Python for "Beginners"'
 
-course = '''
-Hello this is 
-
-Python for "Beginners"
-
-Learning for Aumin
-'''
+# course = '''
+# Hello this is
+#
+# Python for "Beginners"
+#
+# Learning for Aumin
+# '''
 
 
 print(course)
-print(course)
+print(course[-2]) #s
+print(course[0]) # p
+print(course[0:3]) #Pyt
+print(course[:9]) #Python fo # Default start 0
+
+print(course[1:]) # ython for "Beginners" # default end string end
