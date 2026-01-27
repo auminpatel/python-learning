1c96c064e7045d1d764982884a1b3989ff1b87ca
Aumin Patel <auminpatel007@gmail.com>
2026-01-27 23:53:32 +0530

    chore: strings

---
+++ stringMethods.py
@@ -0,0 +1,14 @@
+course = 'Python for Beginners'
+print(course.upper())
+print(len(course))
+
+print(course)
+
+print(course.find('f'))
+
+print(course.replace('B', 'F'))
+
+
+
+print('python' in course)
+print('Python' in course)
