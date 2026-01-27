817f83fa72ec683d9565b7d090701140719cc816
Aumin Patel <auminpatel007@gmail.com>
2026-01-28 00:12:05 +0530

    chore: arthematic

---
+++ arthmatic.py
@@ -0,0 +1,46 @@
+print(10+3)
+print(20%3) # modulas
+
+print(20/3)
+print(20//3) #Round number
+print(10 ** 3) #10 to the power of 3
+
+
+x = 3
+x *= 3 # x+=3  agumented operaton same can be done for subration and multiplcation
+print(x)
+
+
+
+# operator precedence
+
+x = 10 + 3**2 *2
+print(x) #28
+
+#parathesis
+#exponentiation 2**3
+#multipcation or Division
+#addition or subtration
+
+
+x = (2+3) * 10 - 3
+print(x) # 47
+
+
+
+
+x=2.9
+
+print(round(x))
+
+
+x = -2.9
+print(round(x))
+
+
+
+#math module
+import math
+
+print(math.ceil(2.8))
+print(math.floor(2.8))
