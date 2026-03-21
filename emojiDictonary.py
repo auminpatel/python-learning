f40e9257616387bf4b587e573cbea231186548b1
Aumin Patel <auminpatel007@gmail.com>
2026-03-21 23:09:23 +0530

    chore: functions

--- emojiDictonary.py
+++ emojiDictonary.py
@@ -1,15 +1,18 @@
-from phoneMumber import output
+def emojiStatement(message):
+    words = message.split(" ")
+    emojiDict = {
+        ':)': "😃",
+        ":(": "😞"
+    }
+
+    output = ''
+
+    for word in words:
+        output += emojiDict.get(word, word) + ' '
+    return output
+
 
 message = input(">")
-words = message.split(" ")
-emojiDict = {
-    ':)': "😃",
-    ":(": "😞"
-}
 
-output = ''
 
-for word in words:
-    output += emojiDict.get(word, word) + ' '
-
-print(output)
+print(emojiStatement(message))
